4630c454495097800f691f7891ce86c57733ba4e
Frederick Swadling <frederick.swadling@l-p-a.com>
2021-01-06 14:43:08 +0100

    Added segmented sieve. Performance in debug mode down to around 4.5 seconds

--- CSharpSieve/CSharpSieve/Program.cs
+++ CSharpSieve/CSharpSieve/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Diagnostics;
+using System.Linq;
 
 namespace CSharpSieve
 {
     class Program
     {
-        static int ToNumList(BitArray bitArray, int[] buffer)
+        static int ToNumList(BitArray bitArray, int[] buffer, int offset = 0, int nPrimesSoFar = 0)
         {
             var len = bitArray.Length;
             int j = 0;
@@ -15,7 +16,7 @@ namespace CSharpSieve
                 var isPrime = bitArray.Get(i);
                 if (isPrime)
                 {
-                    buffer[j] = i + 1;
+                    buffer[j + nPrimesSoFar] = i + 1 + offset;
                     j++;
                 }
             }
@@ -26,13 +27,13 @@ namespace CSharpSieve
         static int Sieve(int topNumber, int[] buffer)
         {
             var bitArray = new BitArray(topNumber, true);
-            var sqrtNum = Math.Sqrt((double)topNumber);
+            var sqrtNum = (int)Math.Sqrt(topNumber);
 
-            for (int i = 2; i < sqrtNum; i++)
+            for (int i = 2; i <= sqrtNum; i++)
             {
                 if (bitArray[i - 1])
                 {
-                    for (int j = i * i; j < topNumber; j += i)
+                    for (int j = i * i; j <= topNumber; j += i)
                     {
                         bitArray.Set(j - 1, false);
                     }
@@ -44,10 +45,63 @@ namespace CSharpSieve
 
         static int[] Sieve(int topNumber)
         {
+            if (topNumber <= 0)
+                throw new Exception("No.");
+
             var buffer = new int[topNumber];
             var numberOfPrimes = Sieve(topNumber, buffer);
             var primes = buffer[0..numberOfPrimes];
             return primes;
+        }
+
+        static int[] SegmentedSieve(int topNumber, int segmentLength)
+        {
+            if (segmentLength > topNumber)
+                return Sieve(topNumber);
+
+            var primes = new int[(int)topNumber];
+            var nPrimes = Sieve(segmentLength, primes);
+
+            var nSegments = topNumber / segmentLength;
+            if (topNumber % segmentLength > 0)
+                nSegments++;
+
+            // As the first is already done
+            nSegments--;
+
+            var segment = new BitArray(segmentLength, true);
+
+            for (int i = 0; i < nSegments; i++)
+            {
+                var lowerBound = (i + 1) * segmentLength + 1;
+                var upperBound = lowerBound + (segmentLength - 1);
+                var sqrtUpper = (int)Math.Sqrt(upperBound);
+
+                for (int j = 1; j < primes.Length; j++)
+                {
+                    var prime = primes[j];
+
+                    if (prime > sqrtUpper)
+                        break;
+
+                    var n = lowerBound / prime;
+                    if (n * prime < lowerBound)
+                        n++;
+
+                    var primeMultipleInRange = n * prime;
+                    while (primeMultipleInRange <= upperBound)
+                    {
+                        var index = primeMultipleInRange - lowerBound;
+                        segment.Set((int)index, false);
+                        primeMultipleInRange += prime;
+                    }
+                }
+
+                nPrimes += ToNumList(segment, primes, (lowerBound - 1), nPrimes);
+                segment.SetAll(true);
+            }
+
+            return primes[0..nPrimes];
         }
 
         static void Main(string[] args)
@@ -55,7 +109,8 @@ namespace CSharpSieve
             var sw = new Stopwatch();
             sw.Start();
             int target = 500000000;
-            var primes = Sieve(target);
+           // var primes2 = Sieve(target);
+            var primes = SegmentedSieve(target, 1000000);
             sw.Stop();
 
             Console.WriteLine(sw.Elapsed.TotalMilliseconds);
